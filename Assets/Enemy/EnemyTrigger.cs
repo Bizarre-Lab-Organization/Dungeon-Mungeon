e87ad70da1f11fc6e3bb6bcf3684aa696093401f
IlVesAr <ilian2005tttt@gmail.com>
2022-03-12 22:45:25 +0200

    installed A* pathfinding; no pathfinding; passive enemies; ranged enemies

---
+++ Assets/Enemy/EnemyTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonMungeon
+{
+    public class EnemyTrigger : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision) {
+            if(collision.gameObject.tag == "Player"){
+                this.GetComponent<EnemyMovement>().enabled = true;
+            }
+        }
+    }
+}
