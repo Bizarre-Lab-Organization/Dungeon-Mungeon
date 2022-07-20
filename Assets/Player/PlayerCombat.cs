2b7901138ef77188e645f471da4baca298b7c6c1
DaSasho <scarfbg@gmail.com>
2022-07-21 01:55:21 +0300

    Fixed knockback by adding drag to target :>

--- Assets/Player/PlayerCombat.cs
+++ Assets/Player/PlayerCombat.cs
@@ -15,7 +15,7 @@ namespace DungeonMungeon
         private float timeTillAttack;
 
         [SerializeField] private float knockbackStrenght;
-        [SerializeField] private float knockTime;
+        //[SerializeField] private float knockTime;
         void Awake()
         {
             enemyLayers = LayerMask.GetMask("Enemy");
@@ -62,12 +62,13 @@ namespace DungeonMungeon
                 Vector2 difference = target.transform.position - transform.position;
                 difference = difference.normalized * strenght;
                 rb.AddForce(difference, ForceMode2D.Impulse);
-                StartCoroutine(KnockCo(rb));
+                //StartCoroutine(KnockCo(rb));
+                StartCoroutine(SlowOverTime(rb));
                  
             }
         }
 
-        private IEnumerator KnockCo(Rigidbody2D rb)
+        /*private IEnumerator KnockCo(Rigidbody2D rb)
         {
             if(rb != null)
             {
@@ -75,6 +76,32 @@ namespace DungeonMungeon
                 rb.velocity = Vector2.zero;
                 rb.isKinematic = true;
             }
+        }*/
+        private IEnumerator SlowOverTime(Rigidbody2D rb)
+        {
+            /*velo.x = Mathf.Abs(velo.x);
+            velo.y = Mathf.Abs(velo.y);*/
+            while (!rb.velocity.Equals(new Vector2(0, 0))) 
+            {
+                if (rb.velocity.x > 0 && rb.velocity.x < 0.09f) rb.velocity = new Vector2(0, rb.velocity.y);
+                if (rb.velocity.y < 0.09f && rb.velocity.y > 0) rb.velocity = new Vector2(rb.velocity.x,0);
+                if (rb.velocity.x > -0.09f && rb.velocity.x < 0) rb.velocity = new Vector2(0,rb.velocity.y);
+                if (rb.velocity.y > -0.09f && rb.velocity.y < 0) rb.velocity = new Vector2(rb.velocity.x,0);
+
+                /* velo.x--;
+                 velo.x = Mathf.Clamp(velo.x, 0, Mathf.Infinity);
+                 velo.y--;
+                 velo.y = Mathf.Clamp(velo.y, 0, Mathf.Infinity);
+                 Debug.Log(rb.velocity);
+                 Debug.Log(velo);*/
+
+                rb.velocity /= 1.0667f;
+
+                yield return new WaitForEndOfFrame();
+                
+            }
+
+            rb.isKinematic = true;
         }
         private void OnDrawGizmos()
         {
